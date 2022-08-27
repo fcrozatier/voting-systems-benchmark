1d6b51447f89879012f51af0608cfaef91609c8a
fcrozatier <frederic.crozatier@protonmail.com>
2022-08-27 19:35:39 +0200

    add second benchmark

--- scripts/benchmark.py
+++ scripts/benchmark.py
@@ -1,10 +1,23 @@
-from math import ceil
+from math import ceil, sqrt
 
 from scripts.classes import Benchmark
 
 # Todo benchmark functions 1/2**x 1/x 1/log(x)
 
+#
+# 1/x+1 VS 1/2**x
+# Benchmark(
+#     F1=lambda N, k, i: i + ceil(N / (k + 1)),
+#     F2=lambda N, k, i: i + ceil(N / 2**k),
+# ).strong(100, 10_000)
+
+# Benchmark results
+# -----------------
+# Sample size: 10
+# F1 strongly better than F2 at 100%"
+
 Benchmark(
-    F1=lambda N, k, i: i + ceil(N / (k + 1)),
-    F2=lambda N, k, i: i + ceil(N / 2**k),
+    F1=lambda N, k, i: i + ceil(N / sqrt(k + 3)),
+    F2=lambda N, k, i: i + ceil(N / (k + 1)),
+    sample=1,
 ).strong(100, 10_000)
