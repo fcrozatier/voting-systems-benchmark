bb9b1121f99b4599d2238a138f3142765780cd98
fcrozatier <frederic.crozatier@protonmail.com>
2022-08-30 09:07:35 +0200

    add sqrt vs log benchmark

--- scripts/benchmark.py
+++ scripts/benchmark.py
@@ -1,14 +1,12 @@
-from math import ceil, sqrt
+from math import ceil, log, sqrt
 
 from scripts.classes import Benchmark
 
-# Todo benchmark functions 1/log(x)
-
-#
-# Inverse function VS inverse powers of two
+# I. Inverse function VS inverse powers of two
 # Benchmark(
 #     lambda N, k, i: i + ceil(N / (k + 1)),
 #     lambda N, k, i: i + ceil(N / 2**k),
+#     sample=20,
 # ).strong(100, 10_000)
 
 # Benchmark results
@@ -16,16 +14,23 @@ from scripts.classes import Benchmark
 # Sample size: 20
 # F1 strongly better than F2: 100%"
 
-#
-# Inverse square root VS Inverse function
-Benchmark(
-    lambda N, k, i: i + ceil(N / sqrt(k + 3)),
-    lambda N, k, i: i + ceil(N / (k + 1)),
-    sample=10,
-).strong(100, 10_000)
+
+# II. Inverse square root VS Inverse function
+# Benchmark(
+#     lambda N, k, i: i + ceil(N / (1 + sqrt(k))),
+#     lambda N, k, i: i + ceil(N / (1 + k)),
+#     sample=20,
+# ).strong(100, 10_000)
 
 # Benchmark results
 # -----------------
 # Sample size: 20
 # F1 strongly better than F2: 90.0%
 # Average diameter difference when not better: 1
+
+# III. Inverse log VS Inverse square root
+Benchmark(
+    lambda N, k, i: i + ceil(N / (2 + log(k))),
+    lambda N, k, i: i + ceil(N / (1 + sqrt(k))),
+    sample=20,
+).strong(500, 10_000)
