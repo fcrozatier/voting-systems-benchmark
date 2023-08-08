8c3d127dca998229eade6395df7263cf30e1a4ec
fcrozatier <frederic.crozatier@protonmail.com>
2023-08-08 10:11:05 +0200

    refactor bt with vote

--- src/bradley_terry/bradley_terry.py
+++ src/bradley_terry/bradley_terry.py
@@ -1,6 +1,7 @@
 import numpy as np
 
 from src.utilities import *
+from src.votes import Vote
 
 
 def bradley_terry_scores(M: np.ndarray[int], iterations=20, scores=[]):
@@ -33,34 +34,7 @@ def bradley_terry_scores(M: np.ndarray[int], iterations=20, scores=[]):
         return bradley_terry_scores(M, iterations, normalized_new_scores)
 
 
-def BT(ranking: list[int], vote_budget: int, rematch=1, p=0.9):
-    """
-    ranking: the 'true' ranking
-    vote_budget: how many votes to make
-    rematch: how many times to compare a specific pair
-    p: probability to vote according to the true ranking
-    """
-    N = len(ranking)
-    M = np.zeros((N, N))
-
-    remaining_votes = vote_budget
-    while remaining_votes > 0:
-        cycle = random_cycle(N)
-
-        edges = cycle_edges(cycle)
-
-        for i, j in edges:
-            n = rematch
-            while n > 0:
-                n -= 1
-                (l, w) = vote((i, j), ranking, p)
-                remaining_votes -= 1
-                M[w][l] += 1
-
-                if remaining_votes == 0:
-                    break
-        else:
-            continue
-        break
-
-    return ranking_from_scores(bradley_terry_scores(M))
+class BradleyTerry(Vote):
+    def rank(self):
+        self.ranking = ranking_from_scores(bradley_terry_scores(self.comparisons.matrix))
+        return self.ranking
