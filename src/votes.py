5a09ee9a67c9291b8fbfa05139870131e8309a8b
fcrozatier <frederic.crozatier@protonmail.com>
2023-08-08 10:10:19 +0200

    create vote class

--- src/votes.py
+++ src/votes.py
@@ -1,6 +1,43 @@
+from typing import Self
+
 import networkx as nx
 import numpy as np
-from utilities import *
+
+from src.comparisons import *
+from src.utilities import *
+
+
+class Vote:
+    def __init__(self, comparisons: Comparisons, budget: int, rematch=1, p=1) -> None:
+        self.comparisons = comparisons
+        self.budget = budget
+        self.rematch = rematch
+        self.p = p
+        self.true_ranking = random_cycle(comparisons.N)
+
+    def start_vote(self) -> Self:
+        while self.budget:
+            n = self.rematch
+
+            while n > 0:
+                (i, j) = self.comparisons.next_comparison()
+                (loser, winner) = vote((i, j), self.true_ranking, self.p)
+                self.comparisons.record_vote(winner, loser)
+                self.budget -= 1
+                n -= 1
+
+                if self.budget == 0:
+                    break
+            else:
+                continue
+            break
+        return self
+
+    def rank(self) -> list[int]:
+        pass
+
+    def score(self):
+        return top_10(self.true_ranking, self.rank())
 
 
 def initialize_graph(N: int):
