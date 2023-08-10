9e85ad58a5d56684dd3395e292bcac0e19b359f9
fcrozatier <frederic.crozatier@protonmail.com>
2023-08-10 10:49:07 +0200

    implement pairing methods

--- src/comparisons.py
+++ src/comparisons.py
@@ -1,3 +1,4 @@
+from itertools import zip_longest
 from random import sample, shuffle
 
 import networkx as nx
@@ -58,6 +59,7 @@ class ConnectedComponentsComparisons(Comparisons):
         self._components = []
         self._cycle = []
         self._last_node_component_index = 0
+        self._nb_components_memo = []
 
     def next_comparison(self):
         if len(self._cycle) == 0:
@@ -65,6 +67,7 @@ class ConnectedComponentsComparisons(Comparisons):
             self._components = [list(c) for c in nx.strongly_connected_components(self.graph)]
             self._components = sorted(self._components, key=lambda comp: len(comp), reverse=True)
             [shuffle(comp) for comp in self._components]
+            self._nb_components_memo.append(len(self._components))
 
             self._cycle.append(self._components[0].pop())
             self._last_node_component_index = 0
@@ -91,4 +94,84 @@ class ConnectedComponentsComparisons(Comparisons):
 
         # If we couldn't find any non empty component not containing the last node
         self._cycle.append(self._components[self._last_node_component_index].pop())
+        return tuple(self._cycle[-2:])
+
+
+class CCZip(Comparisons):
+    def __init__(self, N) -> None:
+        super().__init__(N)
+        self._components = []
+        self._edges = None
+        self._nb_components_memo = []
+
+    def next_comparison(self):
+        if not self._edges:
+            # Strongly connected components, ordered in decreasing sizes, items shuffled
+            self._components = [list(c) for c in nx.strongly_connected_components(self.graph)]
+            self._components = sorted(self._components, key=lambda comp: len(comp), reverse=True)
+            [shuffle(comp) for comp in self._components]
+            self._nb_components_memo.append(len(self._components))
+
+            # Create a cycle by cycling around the components
+            padded_rows = list(zip_longest(*self._components))
+            cycle = []
+            for row in padded_rows:
+                for node in row:
+                    if node != None:
+                        cycle.append(node)
+
+            self._edges = cycle_edges(cycle).__iter__()
+
+        try:
+            return self._edges.__next__()
+        except StopIteration:
+            self._edges = None
+            return self.next_comparison()
+
+
+class CCSlow(Comparisons):
+    def __init__(self, N) -> None:
+        super().__init__(N)
+        self._components = []
+        self._cycle = []
+        self._last_node_component_index = 0
+        self._nb_components_memo = []
+
+    def next_comparison(self):
+        # Strongly connected components, ordered in decreasing sizes, items shuffled
+        self._components = [list(c) for c in nx.strongly_connected_components(self.graph)]
+        self._components = sorted(self._components, key=lambda comp: len(comp), reverse=True)
+        [shuffle(comp) for comp in self._components]
+
+        if len(self._cycle) == 0:
+            self._nb_components_memo.append(len(self._components))
+
+            self._cycle.append(self._components[0].pop())
+            self._last_node_component_index = 0
+
+        if len(self._cycle) == self.N:
+            comparison = (self._cycle[0], self._cycle[-1])
+            self._cycle = []
+            return comparison
+
+        # If there is only one component
+        if len(self._components) == 1:
+            next_node = list(set(self._components[0]) - set(self._cycle)).pop()
+            self._cycle.append(next_node)
+
+            return tuple(self._cycle[-2:])
+
+        # Pick a random node in the biggest component containing new nodes for the current cycle and not the last node
+        for i, comp in enumerate(self._components):
+            if i == self._last_node_component_index or len(set(comp) - set(self._cycle)) == 0:
+                continue
+
+            next_node = list(set(self._components[i]) - set(self._cycle)).pop()
+            self._cycle.append(next_node)
+            self._last_node_component_index = i
+            return tuple(self._cycle[-2:])
+
+        # If we couldn't find a node in a component not containing the last node
+        next_node = list(set(self._components[self._last_node_component_index]) - set(self._cycle)).pop()
+        self._cycle.append(next_node)
         return tuple(self._cycle[-2:])
--- src/tests/test_comparisons.py
+++ src/tests/test_comparisons.py
@@ -1,3 +1,5 @@
+import pytest
+
 from src.comparisons import *
 
 
@@ -24,6 +26,63 @@ def test_random_expander_comparisons():
 
 def test_connected_components_comparisons():
     comparison_object = ConnectedComponentsComparisons(10)
+
+    # We initially have 10 connected components
+    assert len(list(nx.strongly_connected_components(comparison_object.graph))) == 10
+
+    for i in range(10):
+        (a, b) = comparison_object.next_comparison()
+        comparison_object.record_vote(a, b)
+
+        if i != 9:
+            # At each iterations there are more and more distinct elements in the new cycle of comparisons
+            assert len(set(comparison_object._cycle)) == i + 2
+
+    assert len(comparison_object.graph.edges) == 10
+
+
+def test_cczip():
+    comparison_object = CCZip(10)
+
+    # We initially have 10 connected components
+    assert len(list(nx.strongly_connected_components(comparison_object.graph))) == 10
+
+    for i in range(10):
+        (a, b) = comparison_object.next_comparison()
+        comparison_object.record_vote(a, b)
+
+    assert len(comparison_object.graph.edges) == 10
+
+
+# def test_cczip2():
+#     comparison_object = CCZip(8)
+
+#     for w, l in [
+#         (0, 1),
+#         (1, 2),
+#         (2, 3),
+#         (3, 2),
+#         (3, 4),
+#         (4, 3),
+#         (4, 5),
+#         (2, 5),
+#         (5, 6),
+#         (6, 5),
+#         (1, 6),
+#         (1, 7),
+#         (7, 6),
+#         (7, 0),
+#     ]:
+#         comparison_object.record_vote(w, l)
+
+#     assert len(list(nx.strongly_connected_components(comparison_object.graph))) == 3
+
+#     breakpoint()
+#     comparison_object.next_comparison()
+
+
+def test_ccslow():
+    comparison_object = CCSlow(10)
 
     # We initially have 10 connected components
     assert len(list(nx.strongly_connected_components(comparison_object.graph))) == 10
