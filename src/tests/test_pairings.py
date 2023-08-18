e92aa058ff8024c9acf036cf9d0e12d20b4b7f2c
fcrozatier <frederic.crozatier@protonmail.com>
2023-08-18 17:32:26 +0200

    adjust ccslow test

--- src/tests/test_pairings.py
+++ src/tests/test_pairings.py
@@ -67,33 +67,6 @@ def test_reachability_pairings():
     assert len(nodes_visited) == 10
 
 
-# def test_cczip2():
-#     comparison_object = CCZip(8)
-
-#     for w, l in [
-#         (0, 1),
-#         (1, 2),
-#         (2, 3),
-#         (3, 2),
-#         (3, 4),
-#         (4, 3),
-#         (4, 5),
-#         (2, 5),
-#         (5, 6),
-#         (6, 5),
-#         (1, 6),
-#         (1, 7),
-#         (7, 6),
-#         (7, 0),
-#     ]:
-#         comparison_object.record_vote(w, l)
-
-#     assert len(list(nx.strongly_connected_components(comparison_object.graph))) == 3
-
-#     breakpoint()
-#     comparison_object.next_comparison()
-
-
 def test_ccslow():
     comparison_object = CCSlow(10)
 
@@ -104,8 +77,4 @@ def test_ccslow():
         (a, b) = comparison_object.next_comparison()
         comparison_object.record_vote(a, b)
 
-        if i != 9:
-            # At each iterations there are more and more distinct elements in the new cycle of pairings
-            assert len(set(comparison_object._cycle)) == i + 2
-
-    assert len(comparison_object.graph.edges) == 10
+    assert len(list(nx.strongly_connected_components(comparison_object.graph))) < 10
